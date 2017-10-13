4bd76e0d22e7aa7054021d06cc9d19e14ac0ba84
Jonathan Gray <jsgray@fb.com>
2017-10-13 11:55:42 -0700

    PrintEpisodeReward: print time + steps

--- baselines/common/wrappers.py
+++ baselines/common/wrappers.py
@@ -1,13 +1,28 @@
 import gym
+import time
+import json
 
 class PrintEpisodeReward(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.t_start = None
+
     def _reset(self):
+        if self.t_start is None:
+            self.t_start = time.time()
         self.r = 0
+        self.len = 0
         return self.env.reset()
 
     def _step(self, action):
         obs, r, done, info = self.env.step(action)
         self.r += r
+        self.len += 1
         if done:
-            print('[PrintEpisodeReward] {}'.format(self.r))
+            data = {
+                'time': time.time() - self.t_start,
+                'ep_r': self.r,
+                'ep_len': self.len,
+            }
+            print('[PrintEpisodeReward] ' + json.dumps(data))
         return obs, r, done, info
